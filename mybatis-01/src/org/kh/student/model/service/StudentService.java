51c4cb6377ba6903092db2364ea19b2ec5217f32
고민호 <rhalsgh934@naver.com>
2018-06-26 14:59:05 +0900

    zz

---
+++ mybatis-01/src/org/kh/student/model/service/StudentService.java
@@ -0,0 +1,8 @@
+package org.kh.student.model.service;
+
+import org.kh.student.model.vo.Student;
+
+public interface StudentService {
+	public Student selectStudent();
+	public int insertStudent();
+}
---
+++ mybatis-01/src/org/kh/student/model/vo/Student.java
@@ -0,0 +1,50 @@
+package org.kh.student.model.vo;
+
+import java.sql.Date;
+
+public class Student {
+	private int studentNo;
+	private String studentName;
+	private String studentTel;
+	private String studentEmail;
+	private String studentAddr;
+	private Date regDate;
+	public int getStudentNo() {
+		return studentNo;
+	}
+	public void setStudentNo(int studentNo) {
+		this.studentNo = studentNo;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentTel() {
+		return studentTel;
+	}
+	public void setStudentTel(String studentTel) {
+		this.studentTel = studentTel;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public String getStudentAddr() {
+		return studentAddr;
+	}
+	public void setStudentAddr(String studentAddr) {
+		this.studentAddr = studentAddr;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+}
